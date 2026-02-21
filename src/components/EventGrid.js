2a98744297d39eb732490fc42461399165f69cbb
aaunger2 <aaunger2@illinois.edu>
2026-02-21 16:58:31 -0500

    use data field

--- src/components/EventGrid.js
+++ src/components/EventGrid.js
@@ -5,7 +5,7 @@ import './EventGrid.css';
 export default function EventGrid({query, events}) {
     const safeQuery = query?.toLowerCase() || '';
     console.log("events ", events);
-    const safeEvents = Array.isArray(events) ? events : [];
+    const safeEvents = events && Array.isArray(events.data) ? events : [];
     console.log("safe events ", safeEvents);
 
     const filteredEvents = safeEvents.filter((event) => {
